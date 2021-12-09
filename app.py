ffeebaf98921de94d5846e25dd15a9c999eedec0
nathenjo <nateselectronicrepair@gmail.com>
2021-12-09 11:22:10 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,212 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+# import os
+# import psycopg2
+
+# DATABASE_URL = os.environ['DATABASE_URL']
+
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/chat-app-database'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    image_url = db.Column(db.String(120))
+    children = relationship("Groups")
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.name}"
+
+
+class Groups(db.Model):
+    __tablename__ = 'groups'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    code = db.Column(db.String(7), nullable=False)
+    adminUser = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, name, code, adminUser, user_id):
+        self.name = name
+        self.code = code
+        self.adminUser = adminUser
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"<Group {self.name}"
+
+
+class Messages(db.Model):
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(5000), nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
+    group_id = db.Column(db.String(7), nullable=False)
+
+
+    def __init__(self, text, user_id, group_id):
+        self.text = text
+        self.user_id = user_id
+        self.group_id = group_id
+
+    def __repr__(self):
+        return f"<Message {self.user_id}"
+
+@app.route('/users', methods=['GET'])
+def fetch_users():
+    all_users = Users.query.all()
+    results = [
+        {
+            "id": user.id,
+            "name": user.name,
+            "email": user.email,
+            "password": user.password,
+            "image_url": user.image_url
+        } for user in all_users]
+
+    return {"count": len(results), "users": results}
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    if request.is_json:
+        data = request.get_json()
+        new_user = Users(name=data['name'], email=data['email'], password=data['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return {"message": f"user {new_user.name} has been created successfully."}
+    else:
+        return {"error": "The request payload is not in JSON format"}
+
+
+@app.route('/users/<id>', methods=['GET'])
+def fetch_user(id):
+    user = Users.query.get_or_404(id)
+    results = {
+            "id": user.id,
+            "name": user.name,
+            "email": user.email,
+            "password": user.password,
+            "image_url": user.image_url
+        }
+
+    return {"user": "success", "user": results}
+
+
+@app.route('/edit_user/<user_id>', methods=['PUT'])
+def edit_user(user_id):
+        user = Users.query.get_or_404(user_id)
+        data = request.get_json()
+        user.image_url = data['image_url']
+        db.session.add(user)
+        db.session.commit()
+        return {"message": "user image successfully updated"}
+
+
+@app.route('/groups', methods=['GET'])
+def fetch_groups():
+    all_groups = Groups.query.all()
+    results = [
+        {
+            "id": group.id,
+            "name": group.name,
+            "code": group.code,
+            "adminUser": group.adminUser,
+            "user_id": group.user_id
+        } for group in all_groups]
+
+    return {"count": len(results), "groups": results}
+
+
+@app.route('/add_group', methods=['POST'])
+def add_group():
+    if request.is_json:
+        data = request.get_json()
+        new_group = Groups(name=data['name'], code=data['code'], adminUser=data['adminUser'], user_id=data['user_id'])
+        db.session.add(new_group)
+        db.session.commit()
+        return {"message": f"group {new_group.name} has been created successfully."}
+    else:
+        return {"error": "The request payload is not in JSON format"}
+
+
+@app.route('/groups/<group_id>', methods=['DELETE'])
+def group(group_id):
+    group = Groups.query.get_or_404(group_id)
+    db.session.delete(group)
+    db.session.commit()
+    return {"group": f"Group {group.name} successfully deleted."}
+
+
+
+@app.route('/messages', methods=['GET'])
+def fetch_messages():
+    all_messages = Messages.query.all()
+    results = [
+        {
+            "id": message.id,
+            "text": message.text,
+            "user_id": message.user_id,
+            "group_id": message.group_id
+        } for message in all_messages]
+
+    return {"count": len(results), "messages": results}
+
+    
+@app.route('/messages/<id>', methods=['GET'])
+def fetch_message(id):
+    message = Messages.query.get_or_404(id)
+    results = {
+            "id": message.id,
+            "text": message.text,
+            "user_id": message.user_id,
+            "group_id": message.group_id
+        }
+
+    return {"message": "success", "message": results}
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    if request.is_json:
+        data = request.get_json()
+        new_message = Messages(text=data['text'], user_id=data['user_id'], group_id=data['group_id'])
+        db.session.add(new_message)
+        db.session.commit()
+        return {"message": f"message from user {new_message.user_id} has been created successfully."}
+    else:
+        return {"error": "The request payload is not in JSON format"}
+
+
+@app.route('/messages/<message_id>', methods=['DELETE'])
+def message(message_id):
+    message = Messages.query.get_or_404(message_id)
+    db.session.delete(message)
+    db.session.commit()
+    return {"message": f"Message {message.user_id} successfully deleted."}
+
+@app.route('/')
+def hello():
+    return "Chat App Capstone API Homepage"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
