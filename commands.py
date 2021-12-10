bd6224abe12f55cb312ff405f7adf1e1b51c34f8
nathenjo <nateselectronicrepair@gmail.com>
2021-12-10 00:09:54 -0500

    Added file for creating dabase

--- commands.py
+++ commands.py
@@ -1,8 +1,8 @@
 import click
 from flask.cli import with_appcontext
 
-from .app import db
-from .app import Users, Groups, Messages
+from app import db
+from app import Users, Groups, Messages
 
 @click.command(name='create_tables')
 @with_appcontext
